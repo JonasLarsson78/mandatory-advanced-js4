083ea9c2532b96b60ddc0d70c45cb9f9454e8da9
JonasLarsson78 <jl.7804@gmail.com>
2019-05-15 08:36:52 +0200

    Updates

---
+++ precache-manifest.d10a67270f8ca7085570632264036c65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a44bd4adba904edc08c78f9f541fd5b",
+    "url": "/JonasLarsson78/mandatory-advanced-js4/static/media/back_90_flip.3a44bd4a.jpg"
+  },
+  {
+    "revision": "a2af8aad1c8aa45b3b9b",
+    "url": "/JonasLarsson78/mandatory-advanced-js4/static/js/runtime~main.10a5f7f7.js"
+  },
+  {
+    "revision": "da26a508b875b974321c",
+    "url": "/JonasLarsson78/mandatory-advanced-js4/static/js/main.f18e226c.chunk.js"
+  },
+  {
+    "revision": "48c3593ed5ea80c2d54c",
+    "url": "/JonasLarsson78/mandatory-advanced-js4/static/js/2.5f4c24c4.chunk.js"
+  },
+  {
+    "revision": "da26a508b875b974321c",
+    "url": "/JonasLarsson78/mandatory-advanced-js4/static/css/main.a7f269a4.chunk.css"
+  },
+  {
+    "revision": "171214a513017cc8f6f5ac325578486c",
+    "url": "/JonasLarsson78/mandatory-advanced-js4/index.html"
+  }
+];
